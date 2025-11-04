6f1f5ac6078612a9f9be9a022e4fe0e77133b07b
vaishh1809 <kumarivaishnavi889@gmail.com>
2025-11-04 19:29:54 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1aMO9zYsH3u-IfRd5KJfQRFxTQv7lQ5Bx
 """
 
-!pip install streamlit requests matplotlib -q
+
 !npm install -g localtunnel
 
 # Commented out IPython magic to ensure Python compatibility.
